161383f227c339d36be9765899444d52e3155b34
Correy Everett <correy.everett@gmail.com>
2020-12-06 13:33:12 -0600

    create an int variable called myNumber

--- src/Magic.java
+++ src/Magic.java
@@ -1,3 +1,10 @@
-public class Magic {
 
+// Write a small program that performs a mathematical magic trick! It will involve performing arithmetic operations on an integer that you choose.
+
+public class Magic {
+    public static void main(String[] args) {
+        //Create an int variable called myNumber.
+        // You can change this number to whatever integer you like less than 47000 and greater than -47000. The magic still works.
+        int myNumber = 7;
+    }
 }
