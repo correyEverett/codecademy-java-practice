b9395212c346464e4281f077439935f5591f5e74
Correy Everett <correy.everett@gmail.com>
2020-12-28 21:09:08 -0600

    invoke methods

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -26,7 +26,26 @@ public class SavingsAccount {
     public static void main(String[] args){
         SavingsAccount savings = new SavingsAccount(2000);
 
+        //Check balance:
+        savings.checkBalance();
 
+        //Withdrawing:
+        savings.withdraw(300);
+
+        //Check balance:
+        savings.checkBalance();
+
+        //Deposit:
+        savings.deposit(600);
+
+        //Check balance:
+        savings.checkBalance();
+
+        //Deposit:
+        savings.deposit(600);
+
+        //Check balance:
+        savings.checkBalance();
 
     }
 
