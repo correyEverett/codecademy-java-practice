33d12f0169e05670ee4ed9ecfbe7164ced3c6a3f
Correy Everett <correy.everett@gmail.com>
2020-12-29 10:45:57 -0600

    utilize add method

--- src/Calculator.java
+++ src/Calculator.java
@@ -43,6 +43,8 @@ public class Calculator {
     public static void main(String[] args) {
         Calculator myCalculator = new Calculator();
 
+        System.out.println(myCalculator.add(5, 7));
+
 
     }
 
