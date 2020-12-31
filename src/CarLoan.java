609f2383ca2e71168ca87b8c24c73883b5cc3339
Correy Everett <correy.everett@gmail.com>
2020-12-31 12:18:46 -0600

    inside the else statement add field - interest

--- src/CarLoan.java
+++ src/CarLoan.java
@@ -14,6 +14,7 @@ public class CarLoan {
             int remainingBalance = carLoan - downPayment;
             int months = loanLength * 12;
             int monthlyBalance = remainingBalance / months;
+            int interest = monthlyBalance * interestRate / 100;
 
         }
     }
