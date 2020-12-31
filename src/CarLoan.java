5b1b5b869fc7f6617f77a5831f1aeb764beb7767
Correy Everett <correy.everett@gmail.com>
2020-12-31 12:14:21 -0600

    add conditional logic that checks if the downpayment is greater than the loan

--- src/CarLoan.java
+++ src/CarLoan.java
@@ -8,6 +8,8 @@ public class CarLoan {
 
         if(loanLength <= 0 || interestRate <=0) {
             System.out.println("Error! You must take out a valid car loan.");
+        } else if(downPayment >= carLoan) {
+            System.out.println("The car can be paid in full.");
         }
     }
 }
