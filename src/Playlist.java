bb82f9505f77e2a7882849bdda6cba7d981c6681
Correy Everett <correy.everett@gmail.com>
2021-01-05 22:19:24 -0600

    add songs to the desertIslandPlaylist ArrayList

--- src/Playlist.java
+++ src/Playlist.java
@@ -5,6 +5,13 @@ class Playlist {
     public static void main(String[] args) {
         ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
 
+        desertIslandPlaylist.add("Confessions");
+        desertIslandPlaylist.add("Coming of Age");
+        desertIslandPlaylist.add("Dance with the Devil");
+        desertIslandPlaylist.add("Love In This Club");
+        desertIslandPlaylist.add("Renagade");
+        desertIslandPlaylist.add("Charlene");
+
 
     }
 
