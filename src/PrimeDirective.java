eafb3ea476f96e8769d4a16bbb3140fe26e1ef4f
Correy Everett <correy.everett@gmail.com>
2021-01-10 21:48:39 -0600

    create method - isPrime()

--- src/PrimeDirective.java
+++ src/PrimeDirective.java
@@ -3,5 +3,27 @@ import java.util.ArrayList;
 
 class PrimeDirective {
 
+    // isPrime();
+    public boolean isPrime(int number) {
+
+        if(number == 2) {
+            System.out.println(number + " is the smallest prime number!");
+            return true;
+        } else if(number < 2) {
+            System.out.println(number + " is less than 2, therefore it is not a prime number!");
+            return false;
+        }
+
+        for(int i = 2; i < number; i++) {
+            if(number % i == 0) {
+                System.out.println(number + " is not a prime number!");
+                return false;
+            }
+        }
+
+        System.out.println(number + " is a prime number!");
+        return true;
+    }
+
 
 }
