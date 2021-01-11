30f02f7e62ec0e72111d64eb3d182fd6de187f5b
Correy Everett <correy.everett@gmail.com>
2021-01-10 21:51:17 -0600

    create method - main()

--- src/PrimeDirective.java
+++ src/PrimeDirective.java
@@ -40,5 +40,13 @@ class PrimeDirective {
         return primes;
     }
 
+    // main();
+    public static void main(String[] args) {
+        PrimeDirective pd = new PrimeDirective();
+        int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+
+        //System.out.println(pd.isPrime(6));
+        pd.onlyPrimes(numbers);
+    }
 
 }
