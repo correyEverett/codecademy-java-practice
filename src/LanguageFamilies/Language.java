65d02c43246a35d77812584618565d282653cc0a
Correy Everett <correy.everett@gmail.com>
2021-01-18 15:23:52 -0600

    create method - main()

--- src/LanguageFamilies/Language.java
+++ src/LanguageFamilies/Language.java
@@ -19,4 +19,12 @@ public class Language {
                 " The language follows the word order: " + this.wordOrder + ".");
     }
 
+    public void getInfo() {
+        System.out.println(this.name + " is spoken by " + this.numSpeakers +  " people mainly in " + this.regionSpoken + "." +
+                " The language follows the word order: " + this.wordOrder + ".");
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
