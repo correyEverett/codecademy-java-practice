869c5e4a67003d8dc08a793b3b84f108c67c3f79
Correy Everett <correy.everett@gmail.com>
2021-01-18 15:21:38 -0600

    create constructor - Language

--- src/LanguageFamilies/Language.java
+++ src/LanguageFamilies/Language.java
@@ -7,4 +7,11 @@ public class Language {
     protected String regionSpoken;
     protected String wordOrder;
 
+    Language(String language, int speakerCount, String locations, String syntax) {
+        this.name = language;
+        this.numSpeakers = speakerCount;
+        this.regionSpoken = locations;
+        this.wordOrder = syntax;
+    }
+
 }
