68ab05bb55f21e0f9aaa2b41405108a504b3ece6
Correy Everett <correy.everett@gmail.com>
2021-01-18 15:28:26 -0600

    build constructor for class - Mayan

--- src/LanguageFamilies/Mayan.java
+++ src/LanguageFamilies/Mayan.java
@@ -2,4 +2,9 @@ package LanguageFamilies;
 
 public class Mayan extends Language {
 
+    Mayan(String language, int speakerCount) {
+
+        super(language, speakerCount, "Central America", "verb-object-subject");
+
+    }
 }
