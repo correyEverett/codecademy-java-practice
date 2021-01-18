e1700099b737e66e0a09bfb19657d374580afa9b
Correy Everett <correy.everett@gmail.com>
2021-01-18 15:33:57 -0600

    build constructor for class - SinoTibetan with conditional formatting

--- src/LanguageFamilies/SinoTibetan.java
+++ src/LanguageFamilies/SinoTibetan.java
@@ -2,5 +2,13 @@ package LanguageFamilies;
 
 public class SinoTibetan extends Language {
 
+    SinoTibetan(String language, int speakerCount) {
+
+        super(language, speakerCount, "Asia","subject-object-verb");
+        if(language.contains("Chinese")) {
+            this.wordOrder = "subject-verb-object";
+        }
+
+    }
 
 }
