cfc2c32285964b62fa733edd5987928af93f4bf1
Correy Everett <correy.everett@gmail.com>
2021-01-19 20:04:57 -0600

    add the provided 2D array

--- src/Modifying.java
+++ src/Modifying.java
@@ -1,2 +1,12 @@
+import java.util.Arrays;
 public class Modifying {
+    public static void main(String[] args) {
+        // Using the provided 2D array
+        int[][] intMatrix = {
+                {1, 1, 1, 1, 1},
+                {2, 4, 6, 8, 0},
+                {9, 8, 7, 6, 5}
+        };
+
+    }
 }
